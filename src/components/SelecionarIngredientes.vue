fb943e54d4fe04b2d47e8e8f6ed21e2aa94b133c
Carlos Silva <carlos.david.ftb@hotmail.com>
2024-02-03 09:57:33 -0300

    Função de adicionar e remover ingredientes

--- src/components/SelecionarIngredientes.vue
+++ src/components/SelecionarIngredientes.vue
@@ -1,38 +1,49 @@
 <script lang="ts">
 import { obterCategorias } from '@/http/index';
-import type ICategoria from "@/interfaces/ICategoria";
-export default {
-    data() {
-        return {
-            categorias: [] as ICategoria[]
-        }
-    },
-    async created () {
-      this.categorias = await obterCategorias();
-    }
-}
+import type ICategoria from '@/interfaces/ICategoria';
+import CardCategoria from './CardCategoria.vue';
+import BotaoPrincipal from './BotaoPrincipal.vue';
 
+export default {
+  data() {
+    return {
+      categorias: [] as ICategoria[]
+    };
+  },
+  async created() {
+    this.categorias = await obterCategorias();
+  },
+  components: { CardCategoria, BotaoPrincipal },
+  emits: ['adicionarIngrediente', 'removerIngrediente']
+}
 </script>
 
 <template>
-    <section class="selecionar-ingredientes">
-        <h1 class="cabecalho titulo-ingredientes">Ingredientes</h1>
-        <p class="paragrafo-lg instrucoes">
-            Selecione abaixo os ingredientes que você quer usar nessa receita.
-        </p>
+  <section class="selecionar-ingredientes">
+    <h1 class="cabecalho titulo-ingredientes">Ingredientes</h1>
 
-        <ul class="categorias">
-            <li v-for="categoria in categorias" :key="categoria.nome">
-                {{ categoria.nome }}
+    <p class="paragrafo-lg instrucoes">
+      Selecione abaixo os ingredientes que você quer usar nesta receita:
+    </p>
 
-            </li>
+    <ul class="categorias">
+      <li v-for="categoria in categorias" :key="categoria.nome">
+        <CardCategoria
+          :categoria="categoria"
+          @adicionar-ingrediente="$emit('adicionarIngrediente', $event)"
+          @remover-ingrediente="$emit('removerIngrediente', $event)"
+        />
+      </li>
+    </ul>
 
-        </ul>
-        <p class="paragrafo dica">
-            *Atenção, consideramos que você tem em casa sal, pimenta e água
-        </p>
-    </section>
+    <p class="paragrafo dica">
+      *Atenção: consideramos que você tem em casa sal, pimenta e água.
+    </p>
+
+    <BotaoPrincipal texto="Buscar receitas!" />
+  </section>
 </template>
+
 <style scoped>
 .selecionar-ingredientes {
   display: flex;
